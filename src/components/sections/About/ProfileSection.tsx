d15bbeea85487879ab38bcb60f9aff9bd698f2e6
Andreu <asanchez@codiara.com>
2025-05-01 12:32:03 +0200

    modify img profile

--- src/components/sections/About/ProfileSection.tsx
+++ src/components/sections/About/ProfileSection.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import profileImg from '../../../assets/img/andreu.jpg';
 
 export const ProfileSection: React.FC = () => {
   return (
@@ -6,7 +7,7 @@ export const ProfileSection: React.FC = () => {
       <div className="absolute -inset-1 bg-gradient-purple rounded-xl blur-lg opacity-50 group-hover:opacity-75 transition duration-500"></div>
       <div className="relative aspect-square overflow-hidden rounded-xl border-2 border-purple">
         <img 
-          src="https://images.pexels.com/photos/2004161/pexels-photo-2004161.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1" 
+          src={profileImg} 
           alt="Profile" 
           className="w-full h-full object-cover transition-transform duration-500 group-hover:scale-105"
         />
